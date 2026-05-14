f4440ba52c4a83f435001b171511c41634baba77
niluferbagevi-gif <niluferbagevi@gmail.com>
2026-05-14 05:18:30 +0300

    Restore strict test config helper coverage

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -28,9 +28,11 @@ def test_make_test_config_uses_spec_set_with_full_app_config(
         AI_PROVIDER = "ollama"
         OLLAMA_URL = "http://localhost:11434"
         OLLAMA_CONTEXT_MAX_CHARS = 12000
+        OLLAMA_KEEP_ALIVE = "30m"
         LLM_MAX_RETRIES = 1
         LLM_RETRY_BASE_DELAY = 0.1
         LLM_RETRY_MAX_DELAY = 0.2
+        SWARM_TASK_TIMEOUT_SECONDS = 60
         ENABLE_SEMANTIC_CACHE = True
         SEMANTIC_CACHE_THRESHOLD = 0.9
         SEMANTIC_CACHE_TTL = 60
