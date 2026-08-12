949e698e8f2baaf354a5f3124f3e1082f6d93f39
niluferbagevi-gif <niluferbagevi@gmail.com>
2026-08-12 03:29:31 +0300

    Pin install_sidar.sh embedded module commit to f9c4574149d8f6049f0b27c90ef9c5af1ad46dd0

--- install_sidar.sh
+++ install_sidar.sh
@@ -182,7 +182,7 @@ load_remote_script_checksums() {
 load_remote_script_checksums
 
 SIDAR_INSTALLER_EMBEDDED_SOURCE_REF="main"
-SIDAR_INSTALLER_EMBEDDED_SOURCE_COMMIT="b20d818728a4152a120c70b04409f2f1ae4adc1c"
+SIDAR_INSTALLER_EMBEDDED_SOURCE_COMMIT="f9c4574149d8f6049f0b27c90ef9c5af1ad46dd0"
 
 sidar_truthy_early_bool() {
     local raw="${1:-}"
